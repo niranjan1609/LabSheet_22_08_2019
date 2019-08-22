2e86361efcf580a37d5c856b8c0c81a22d35a71a
Niranjan <39219313+niranjan1609@users.noreply.github.com>
2019-08-22 10:33:17 +0530

    Add files via upload

---
+++ program4.s
@@ -0,0 +1,13 @@
+	.data
+	.text
+	.globl main
+main:
+	li $t6, 2		
+	li $t7, 10		
+	li $t0, 0		
+loop:	ble $t6, $0, exit	
+	add $t0, $t0, $t7	
+	addi $t6, $t6, -1	
+	b loop
+exit:	li $v0, 10
+	syscall
